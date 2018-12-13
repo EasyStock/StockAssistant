4d79f18a172d37f5fa64d5b1c7646d35b50f3f3e
robin huang <yuchonghuang@sina.cn>
2018-12-13 21:53:18 +0800

    update

--- src/RongZiRongQuan/RongZiRongQuanMgr.py
+++ src/RongZiRongQuan/RongZiRongQuanMgr.py
@@ -5,7 +5,7 @@ Created on 2018年12月6日
 @author: robin huang(yuchonghuang@sina.cn)
 '''
 from RongZiRongQuan.RongZiRongQuan_SH import CRongZiRongQuan_SH
-from TradingDays import getTradingDaysWithoutSperate
+from TradingDays import getTradingDaysWithoutSperate, getTradingDaysWithSperate
 from Configuration import RongZiRongQuan_DefaultFolder
 from RongZiRongQuan.RongZiRongQuan_SZ import CRongZiRongQuan_SZ
 
@@ -14,18 +14,23 @@ def GetAllRongZhiRongQuan(_from=None, _to=None, path=None):
 
     if path is None:
         path = RongZiRongQuan_DefaultFolder
-    allDates = getTradingDaysWithoutSperate(_from, _to)
-    if allDates is None or len(allDates) == 0:
+    allDates_SH = getTradingDaysWithoutSperate(_from, _to)
+    allDates_SZ = getTradingDaysWithSperate(_from, _to)
+    if allDates_SH is None or len(allDates_SH) == 0:
         print 'date error!'
+        return False
+
+    if allDates_SZ is None or len(allDates_SZ) == 0:
+        print 'date error 1!'
         return False
 
     folder_SH = u'%s/沪市/' % (path)
     folder_SZ = u'%s/深市/' % (path)
     sh = CRongZiRongQuan_SH()
-    sh.getRongZiRongQuanByDates(allDates, folder_SH)
+    sh.getRongZiRongQuanByDates(allDates_SH, folder_SH)
 
     sz = CRongZiRongQuan_SZ()
-    sz.getRongZiRongQuanByDates(allDates, folder_SZ)
+    sz.getRongZiRongQuanByDates(allDates_SZ, folder_SZ)
 
 
 if __name__ == '__main__':
